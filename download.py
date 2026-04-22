b922337abe3f17030f03a400dad6e18b231cc40d
Sublevel1384 <158116984+Sublevel1384@users.noreply.github.com>
2026-04-22 10:38:06 -0400

    Update download URL in download.py
    Moved file to separate SharePoint directory.

--- download.py
+++ download.py
@@ -1,7 +1,7 @@
 
 import requests
 
-url = "https://beltonene-my.sharepoint.com/:x:/p/npacheco/Ef3m6BwbgLRBmORQKDB69EsBmem7EzOY9k-jg7ibS4vd5g?e=Wh4oCe&download=1"
+url = "https://beltonene-my.sharepoint.com/:x:/p/helpdesk/IQCIapO9xEkDRqlMMrqazoMCARaqJgqnqqIzLBani5yd-tA?e=djfmzH&download=1"
 output_file = "downloaded_file.xlsx"
 
 response = requests.get(url)
